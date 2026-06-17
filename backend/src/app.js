8ea9fa9c835a6caf6ee021a0c6acb4853a2e9dcd
Iheb Haouech <ihebhaouech27@gmail.com>
2026-06-17 03:23:26 +0100

    Add production domain to CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175', 'http://127.0.0.1:5173', 'http://127.0.0.1:5174', 'http://127.0.0.1:5175'],
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175', 'http://127.0.0.1:5173', 'http://127.0.0.1:5174', 'http://127.0.0.1:5175', 'https://djerbahouch.fr', 'http://djerbahouch.fr'],
     credentials: true,
   })
 );
